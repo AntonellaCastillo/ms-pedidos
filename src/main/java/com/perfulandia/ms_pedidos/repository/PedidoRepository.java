c20a32b6458ebd163906c64c020721cda8817618
Antonella Castillo <ant.castillos@duocuc.cl>
2026-05-23 10:57:30 -0400

    fix: agrgar codigo pedido repository

--- src/main/java/com/perfulandia/ms_pedidos/repository/PedidoRepository.java
+++ src/main/java/com/perfulandia/ms_pedidos/repository/PedidoRepository.java
@@ -1,5 +1,16 @@
 package com.perfulandia.ms_pedidos.repository;
 
-public class PedidoRepository {
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import com.perfulandia.ms_pedidos.model.Pedido;
 
+@Repository
+public interface PedidoRepository extends JpaRepository<Pedido, Long> {
+    
+    // Buscar pedidos por cliente
+    List<Pedido> findByIdCliente(Long idCliente);
+    
+    // Buscar pedidos por estado
+    List<Pedido> findByEstado(String estado);
 }
